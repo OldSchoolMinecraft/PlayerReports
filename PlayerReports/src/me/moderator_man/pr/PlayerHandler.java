8f83de1af1bb4870a1d2303b6a86a32c142fda4e
CodeBasePW <coderatorman@gmail.com>
2020-02-03 00:01:22 -0800

    Added eclipse project files

--- PlayerReports/src/me/moderator_man/pr/PlayerHandler.java
+++ PlayerReports/src/me/moderator_man/pr/PlayerHandler.java
@@ -43,7 +43,8 @@ public class PlayerHandler extends PlayerListener
 		{
 			public void run()
 			{
-				sender.sendMessage(msg);
+				if (sender != null)
+					sender.sendMessage(msg);
 			}
 		}, 1);
 	}
